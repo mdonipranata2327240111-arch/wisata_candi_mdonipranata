a1f6b1653b48a3e70683effd01f8c501b2ea1003
mdonipranata2327240111-arch <mdonipranata_2327240111@mhs.mdp.ac.id>
2025-11-03 13:05:28 +0700

    updateprofilescreenslide13

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/ProfileScreen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: ProfileScreen(),
     );
   }
 }
---
+++ lib/screens/ProfileScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  // TODO: 1. Deklarasikan variabel yang dibutuhkan
+  bool isSignedIn = false;
+  String fullName = '';
+  String userName = '';
+  int favoriteCandiCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.deepPurple,
+          ), // Container
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            // TODO: 2. Buat bagian ProfileHeader yang berisi gambar profil
+            Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.deepPurple, width: 2),
+                    shape: BoxShape.circle,
+                  ), // BoxDecoration
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('images/placeholder_image.png'),
+                  ), // CircleAvatar
+                ), // Container
+              ],
+            ), // Stack
+
+            // TODO: 3. Buat bagian ProfileInfo yang berisi info profil
+            // TODO: 4. Buat ProfileActions yang berisi TextButton sign in/out
+          ],
+        ),
+
